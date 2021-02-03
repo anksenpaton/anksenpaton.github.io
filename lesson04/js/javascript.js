b758ebd7aead7725802967470a9bdd10ded53b86
anksenpaton <anksenpaton@yahoo.com>
2021-02-03 02:48:35 -0800

    m

--- lesson04/js/javascript.js
+++ lesson04/js/javascript.js
@@ -2,6 +2,17 @@
 function getLastUpdateDate() {
     var dt = new Date();
    /* document.getElementById("updateDate").innerHTML =	dt.toLocaleDateString();*/
-    document.getElementById("updateDate").innerHTML =	dt.toDateString();
+    document.getElementById("updateDate").innerHTML = dt.toDateString();
 }
 
+const hambutton = document .querySelector();
+const mainnav = document .querySelector(navigation);
+hambutton .addEventListener('click'()=6);
+{
+    mainnav.classList.toggle(responsive)
+},false);
+window.onresize=()=>{if(window.innerWidth>760);
+{
+    mainnav.classList.remove('responsive')
+}
+};
---
+++ lesson04/start-navigation/js/scripts.js
@@ -0,0 +1,18 @@
+
+function getLastUpdateDate() {
+    var dt = new Date();
+   /* document.getElementById("updateDate").innerHTML =	dt.toLocaleDateString();*/
+    document.getElementById("updateDate").innerHTML = dt.toDateString();
+}
+
+const hambutton = document .querySelector();
+const mainnav = document .querySelector(navigation);
+hambutton .addEventListener('click'()=6);
+{
+    mainnav.classList.toggle(responsive)
+},false);
+window.onresize=()=>{if(window.innerWidth>760);
+{
+    mainnav.classList.remove('responsive')
+}
+};
