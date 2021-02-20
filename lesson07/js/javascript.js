d3a8c5e4ca68b1bd0100f335e84c4a8e719d96c5
anksenpaton <anksenpaton@yahoo.com>
2021-02-20 12:58:12 -0800

    m

--- lesson07/js/javascript.js
+++ lesson07/js/javascript.js
@@ -147,5 +147,31 @@ function getLastUpdateDate() { /* returns something like: Wednesday January 1st 
     document.getElementById("bannerAdd").innerHTML = "&nbsp;";
   
  }
-
+ const config = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 1
+  }
+  
+  const images = document.querySelectorAll('[data-src]')
+  
+  let observer = new IntersectionObserver(function (entries, self) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        preloadImage(entry.target)
+        self.unobserve(entry.target)
+      }
+    })
+  }, config)
+  
+  images.forEach(image => {
+    observer.observe(image)
+  })
+  
+  function preloadImage (img) {
+    const src = img.getAttribute('data-src')
+    if (!src) {
+      return
+    }
+    img.src = src
+  }
 }
