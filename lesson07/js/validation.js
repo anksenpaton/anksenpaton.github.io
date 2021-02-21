50a1d0e2fa073f594055155e5e02ccbc9dc161f2
anksenpaton <anksenpaton@yahoo.com>
2021-02-20 18:44:55 -0800

    m

---
+++ lesson07/js/validation.js
@@ -0,0 +1,51 @@
+function pwCompare() {
+
+    var pw1 = document.getElementById('pw1').value;
+    var pw2 = document.getElementById('pw2').value; 
+    var enabled = false;
+
+
+    /* if pw1 is the same as pw2 then enable the submit button */
+    if (pw1 == pw2) {
+        document.getElementById("pwMessage").innerHTML = "Match"
+	enabled = true;
+    } else {
+        document.getElementById("pwMessage").innerHTML = "Doesn't Match"
+	enabled = false;
+    }
+    if ((pw1  == pw2) && (pw2.length < 7)) {
+        document.getElementById("pwMessage").innerHTML = "Too short. Min 7 characters."
+	enabled = false;
+    }
+
+    if (enabled) {
+        document.getElementById('submit').disabled = false; /* submit button enabled */
+    }
+}
+
+function charCheck() {
+
+    var input1 = document.getElementById('input1').value;
+    var enabled = false;
+
+
+    /* if pw1 is the same as pw2 then enable the submit button */
+    if (input1.includes("!") || 
+        input1.includes("@") ||                    
+        input1.includes("#") ||                    
+        input1.includes("(") ||                    
+        input1.includes(")") ||                    
+        input1.includes("$") ){
+	enabled = true;
+    } else {
+	enabled = false;
+    }
+
+    if (enabled) {
+        document.getElementById('submit1').disabled = false; /* submit button enabled */
+    } else {
+        document.getElementById('submit1').disabled = true; /* submit button disabled */
+    }
+
+
+}
