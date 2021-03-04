9a501861500864ed031f4073e7142ad654004cff
anksenpaton <anksenpaton@yahoo.com>
2021-03-03 21:48:30 -0800

    m

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -6,21 +6,24 @@ const requestURL = 'js/prophets.json';
 fetch(requestURL)
     .then(function (response) {
         return response.json();
-       
+        
+        
 
     })
     .then(function (jsonObject) {
         console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];
-        
+        const prophets = jsonObject["prophets"];
+        var arr = ["prophets"];
     }
     )
     ;
 
-    for (let i = 0; i < prophets.length(); i++) {
+    for (let i = 0; i < prophets.length; i++) {
 
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let p = document.createElement('p');
+    let img = document.createElement('img');
 
     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
