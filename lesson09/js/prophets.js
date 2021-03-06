644ac56310d7eccf96a871e8342d66d74823323a
anksenpaton <anksenpaton@yahoo.com>
2021-03-05 22:29:18 -0800

    m

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -9,32 +9,30 @@ fetch(requestURL)
       const prophets = dataObject['prophets'];
       for (let i = 0; i < prophets.length; i++) {
          let card = document.createElement('section');
-         
+
          let h2 = document.createElement('h2');
          h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
          card.appendChild(h2);
-        
+
          let p1 = document.createElement('p1');
          p1.textContent = 'Date of Birth: ' + prophets[i].birthdate + ' ';
          card.appendChild(p1);
-                
+
 
          let p2 = document.createElement('p2');
          p2.textContent = `Place of Birth:${prophets[i].birthplace} `;
          card.appendChild(p2);
-        
+
 
          let img = document.createElement('img');
          img.setAttribute('src', prophets[i].imageurl);
-         
-         img.alt= `${prophets[i].name} ${prophets[i].lastname}`;
-/*
-         img.alt= prophets[i].name + ' ' + prophets[i].lastname;
-*/
-         img.style.width= '200px';
+
+         img.alt = `${prophets[i].name} ${prophets[i].lastname}`;
+
+         img.style.width = '200px';
          card.appendChild(img);
-        
-        
+
+
          document.querySelector('div.cards').appendChild(card);
       }
    });
