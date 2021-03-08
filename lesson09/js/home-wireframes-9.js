b1db566b5c14951783b9b1c805a4ea34fca9a1ef
anksenpaton <anksenpaton@yahoo.com>
2021-03-08 12:14:20 -0800

    m

--- lesson09/js/home-wireframes-9.js
+++ lesson09/js/home-wireframes-9.js
@@ -6,28 +6,28 @@ fetch(requestURL)
    })
    .then(function (dataObject) {
       console.table(dataObject);
-      const prophets = dataObject['prophets'];
-      for (let i = 0; i < prophets.length; i++) {
+      const towndata = dataObject['towndata'];
+      for (let i = 0; i < towndata.length; i++) {
          let card = document.createElement('section');
 
          let h2 = document.createElement('h2');
-         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+         h2.textContent = towndata[i].name + ' ' + towndata[i].lastname;
          card.appendChild(h2);
 
          let p1 = document.createElement('p1');
-         p1.textContent = 'Date of Birth: ' + prophets[i].birthdate + ' ';
+         p1.textContent = 'Date of Birth: ' + towndata[i].birthdate + ' ';
          card.appendChild(p1);
 
 
          let p2 = document.createElement('p2');
-         p2.textContent = `Place of Birth:${prophets[i].birthplace} `;
+         p2.textContent = `Place of Birth:${towndata[i].birthplace} `;
          card.appendChild(p2);
 
 
          let img = document.createElement('img');
-         img.setAttribute('src', prophets[i].imageurl);
+         img.setAttribute('src', towndata[i].imageurl);
 
-         img.alt = `${prophets[i].name} ${prophets[i].lastname}`;
+         img.alt = `${towndata[i].name} ${towndata[i].lastname}`;
          
          img.style.width = '200px';
          card.appendChild(img);
