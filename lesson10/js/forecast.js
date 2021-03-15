8e9df981bcc3554056218a8ccb6d5e31544323b8
anksenpaton <anksenpaton@yahoo.com>
2021-03-15 00:07:10 -0700

    m

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,13 +1,37 @@
+
+
+
+let cityID = 5604473;
+let appid = 'cf6255e7d3643d472202cab287a3b281';
+
 const apiURL =
-'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=cf6255e7d3643d472202cab287a3b281'
+'http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=imperial&appid=${appid}';
+
 
  /* Preston ID id=5604473 */
 
 fetch(apiURL)
  .then(response => response.json())
  .then(jsObject => {
-  /* console.log(jsObject)*/
+   console.log(jsObject)
+   let day = 0;
+   conts dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+/*
+let d = new Date(jsobject.list[4].dt_txt);*/
+thefive.forEach( days => {
+  let d = new Date(days.dt_txt);
+console.log(d);
+document.getElementById('dayofweek${day+1}').textContent = dayofWeek[d.getDay()];
+document.getElementById('forecast${day+1}').textContent = jsObject.list[4].main.temp;
+day++
+});
+ });
+
+
+
+
+/*
    document.getElementById('current-temp').textContent = jsObject.main.temp
    document.getElementById('high-temp').textContent = jsObject.main.temp_max
    document.getElementById('wind-chill').textContent = jsObject.main.feels_like
@@ -26,3 +50,4 @@ fetch(apiURL)
    document.getElementById('icon').setAttribute('alt', desc);
 
  })
+ */
