01a2680b19762e3721a62f0916fc6909e893ec2b
anksenpaton <anksenpaton@yahoo.com>
2021-03-15 15:01:04 -0700

    x

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -15,10 +15,7 @@ fetch(apiURL)
    document.getElementById('humidity').textContent = jsObject.main.humidity
    document.getElementById('weather').textContent = jsObject.weather[0].description
 
-
-
-
-
+   document.getElementById('current-temp2').textContent = jsObject.main.temp
    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
    const desc = jsObject.weather[0].description;  // note how we reference the weather array
    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
