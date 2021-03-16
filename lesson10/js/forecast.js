28710c159dd3dfa63134645845da48f272acd9ef
anksenpaton <anksenpaton@yahoo.com>
2021-03-16 14:32:36 -0700

    m

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,57 +1,46 @@
-let cityID = 5604473;  /* Preston ID id=5604473 */
-let appid = 'cf6255e7d3643d472202cab287a3b281';
-
-const apiwURL =
-'https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=imperial&appid=${appid}';
-
-fetch(apiwURL)
- .then(response => response.json())
- .then(jsObject => {
-   console.log(jsObject)
-   let day = 0;
-   const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let cityID_f = 5604473 /* Preston ID id=5604473 */
+let appid_f = '00000000000000000000000'
 
 /*
-let d = new Date(jsobject.list[4].dt_txt);*/
-/*
-thefive.forEach( days => {
-
-  let d = new Date(days.dt_txt);
- 
-console.log(d);
-document.getElementById('dayofweek${day+1}').textContent = dayofWeek[d.getDay()];
-document.getElementById('forecast${day+1}').textContent = jsObject.list[4].main.temp;
-day++
- */
-});
-
-/*
- let img = document.createElement('img');
- img.setAttribute('src', prophets[i].imageurl);
-
- img.alt = `${prophets[i].name} ${prophets[i].lastname}`;
- 
- img.style.width = '200px';
- card.appendChild(img);
+const apiURL_f =
+'https://api.openweathermap.org/data/2.5/forecast?id=${cityID_f}&units=imperial&appid=${appid_f}';
 */
 
-/*
-   document.getElementById('current-temp').textContent = jsObject.main.temp
-   document.getElementById('high-temp').textContent = jsObject.main.temp_max
-   document.getElementById('wind-chill').textContent = jsObject.main.feels_like
-   document.getElementById('wind-speed').textContent = jsObject.wind.speed
-   document.getElementById('humidity').textContent = jsObject.main.humidity
-   document.getElementById('weather').textContent = jsObject.weather[0].description
+const apiURL_f = '/js/forecast.json'
 
-
-
-
-
-   const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-   const desc = jsObject.weather[0].description;  // note how we reference the weather array
-   document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-   document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-   document.getElementById('icon').setAttribute('alt', desc);
-
+fetch(apiURL_f)
+ .then(function (response) {
+   return response.json()
  })
- */
+
+ .then(function (dataObject) {
+   console.log(dataObject)
+   // console.log(dataObject.cnt)
+   for (let i = 0; i < dataObject.cnt; i++) {
+     if (dataObject.list[i].dt_txt.includes('18:00:00')) {
+       let card = document.createElement('section')
+       var date = new Date(dataObject.list[i].dt_txt)
+       const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+       document.querySelector('div.cards').appendChild(card)
+
+       let p1 = document.createElement('p')
+       p1.textContent = dayOfWeek[date.getDay()]
+       card.appendChild(p1)
+
+       const imagesrc = 'https://openweathermap.org/img/w/' + dataObject.list[i].weather[0].icon  + '.png'
+       //p2.textContent = `<img src="${imagesrc}" alt="${dataObject.list[i].weather[0].description}" id="icon">`;
+
+     let img = document.createElement('img')
+     img.setAttribute('src', imagesrc)
+     img.alt = `$dataObject.list[i].weather[0].description`
+     img.setAttribute('id', 'icon')
+     img.style.width = '60px'
+     card.appendChild(img)
+
+       let p3 = document.createElement('p')
+       p3.textContent = dataObject.list[i].main.temp
+       card.appendChild(p3)
+
+     }
+   }
+ })
--- lesson10/js/weatherapi_old.js
+++ lesson10/js/weatherapi_old.js
@@ -1,23 +1,26 @@
-let cityID_w = 5604473;  /* Preston ID id=5604473 */
-let appid_w = '00000000000000000000000000000000000';
-
-/* 'https://api.openweathermap.org/data/2.5/forecast?id=${cityID_w}&units=imperial&appid=${appid_w}';   */
-
-const apiURL_w = '/js/weather.json'
+const apiURL =
+'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=cf6255e7d3643d472202cab287a3b281'
 
  /* Preston ID id=5604473 */
 
-fetch(apiURL_w)
+fetch(apiURL)
  .then(response => response.json())
  .then(jsObject => {
-   // console.log(jsObject)
+  /* console.log(jsObject)*/
 
    document.getElementById('current-temp').textContent = jsObject.main.temp
+   document.getElementById('high-temp').textContent = jsObject.main.temp_max
+   document.getElementById('wind-chill').textContent = jsObject.main.feels_like
+   document.getElementById('wind-speed').textContent = jsObject.wind.speed
+   document.getElementById('humidity').textContent = jsObject.main.humidity
+   document.getElementById('weather').textContent = jsObject.weather[0].description
 
+/*
    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
    const desc = jsObject.weather[0].description;  // note how we reference the weather array
    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
    document.getElementById('icon').setAttribute('alt', desc);
+*/
 
  })
