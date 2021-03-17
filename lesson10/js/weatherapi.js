1f963e5bee010b2800d90b4412e2db823ffff8be
anksenpaton <anksenpaton@yahoo.com>
2021-03-16 18:24:42 -0700

    m

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,18 +3,18 @@ let appid_w = 'cf6255e7d3643d472202cab287a3b281';
 
 /* 'https://api.openweathermap.org/data/2.5/forecast?id=${cityID_w}&units=imperial&appid=${appid_w}';   */
 
-const apiURL_w = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID_w}&units=imperial&appid=${appid_w}`
+const apiURL_w = `https://api.openweathermap.org/data/2.5/weather?id=${cityID_w}&units=imperial&appid=${appid_w}`
 
  /* Preston ID id=5604473 */
 
 fetch(apiURL_w)
  .then(response => response.json())
  .then(jsObject => {
-   // console.log(jsObject)
+    console.log(jsObject)
 
    document.getElementById('current-temp').textContent = jsObject.main.temp
    document.getElementById('high-temp').textContent = jsObject.main.temp_max
-   document.getElementById('wind-chill').textContent = jsObject.main.feels_like
+   document.getElementById('wind-chill').textContent = windChill(jsObject.main.temp,jsObject.wind.speed)
    document.getElementById('wind-speed').textContent = jsObject.wind.speed
    document.getElementById('humidity').textContent = jsObject.main.humidity
    document.getElementById('weather').textContent = jsObject.weather[0].description
--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,11 +1,11 @@
 
 
-function windChill() {
+function windChill(temp,wind) {
 
-    var wind;
-    var temp;
-    temp = 36;
-    wind = 7;
+    //var wind;
+    //var temp;
+    //temp = 36;
+    //wind = 7;
     var chill;
     chill = Math.round((0.0817 * (3.71 * (Math.pow(wind, 0.5)) + 5.81 - 0.25 * wind) * (temp - 91.4) + 91.4));
 
@@ -17,7 +17,8 @@ function windChill() {
     else {
         chill = "N/A";
     }
-    document.getElementById("temp").innerHTML = temp;
-    document.getElementById("wind").innerHTML = wind;
-    document.getElementById("windchill").innerHTML = chill;
+    return chill;
+    //document.getElementById("temp").innerHTML = temp;
+    //document.getElementById("wind").innerHTML = wind;
+   // document.getElementById("windchill").innerHTML = chill;
 }
